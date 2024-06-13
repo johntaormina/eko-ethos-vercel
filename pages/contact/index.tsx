960ecb6624db4a5d3d29b8e61c6ba69b3be9fcf6
johntaormina <jtaor96@gmail.com>
2024-06-13 12:55:32 -0400

    update

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import ContextLayout from "../../layouts/ContextLayout";
 
 const Index = () => {
@@ -6,36 +7,27 @@ const Index = () => {
     <ContextLayout>
       <div className="flex flex-col h-screen">
         <div className="flex flex-row h-full">
-          <div className="w-1/3 bg-gray-200">
-            {/* Map */}
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6115.553614026713!2d-75.17871064986761!3d39.96874255860054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6c7ceaf352c7b%3A0xc1ad932f69f4d963!2sFairmount%2C%20Philadelphia%2C%20PA%2019130!5e0!3m2!1sen!2sus!4v1717698755194!5m2!1sen!2sus"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen={true}
-              loading="lazy"
-            ></iframe>
-          </div>
-          <div className="w-1/3 bg-gray-300">
+          <div className="w-1/2 bg-gray-300">
             {/* Get to know me */}
             <div className="flex flex-col items-center justify-center h-full">
-              <img
+              <Image
                 src="/assets/kelsie.jpg"
                 alt="Headshot"
-                className="w-40 h-40 rounded-full mb-4"
+                width={40}
+                height={40}
+                className="rounded-full mb-4"
               />
               <h2 className="text-xl font-bold">Kelsie Roberton</h2>
               <p className="text-black-600 p-20">Highly motivated professional with experience in advocating and effectively communicating optimal sustainability strategies. Experienced in project management and competent at fostering relationships with both internal and external stakeholders.
               </p>
             </div>
           </div>
-          <div className="w-1/3 bg-gray-400">
+          <div className="w-1/2 bg-gray-400">
             {/* Contact */}
             <div className="flex flex-col items-center justify-center h-full">
               <p className="text-2xl font-bold">Contact Me</p>
-              <p className="text-gray-600">Phone: 630-715-61140</p>
-              <p className="text-gray-600">Email: kelsieroberton@gmail.com</p>
+              <p className="text-gray-600">Phone: +1 630 715 6114</p>
+              <p className="text-gray-600">Email: ekokelsie@gmail.com</p>
             </div>
           </div>
         </div>
--- pages/spotlight/index.tsx
+++ pages/spotlight/index.tsx
@@ -1,40 +1,46 @@
-import React from "react";
-import { Plant } from "../../components/main/plant";
-import ContextLayout from "layouts/ContextLayout";
-import BrandCard from "../../components/main/spotlight/brandcard";
+import React from 'react';
+import { Plant } from '../../components/main/plant';
+import ContextLayout from 'layouts/ContextLayout';
+import BrandCard from '../../components/main/spotlight/brandcard';
 
 const Index = () => {
   return (
-    <div className="bg-repeat bg-[url('/assets/plant-1.jpg')]">
-      <ContextLayout>
-        <div className="spotlight-page grid grid-cols-3 pt-20">
-          <div className="w-full">
-          </div>
-          <div className="grid grid-rows-1">
-            <div className="pb-5">
-            <BrandCard
-              image="assets/brands/dr-bronners.png"
-              name="Dr. Bronners"
-              description="Dr. Bronner's is a renowned brand celebrated for its organic and fair-trade personal care products, especially its versatile liquid and bar soaps. Founded in 1948 by Emanuel Bronner, the company has maintained a strong commitment to environmental sustainability and social responsibility. Their products are made with natural, biodegradable ingredients and come in eco-friendly packaging. Dr. Bronner's is also known for its advocacy on various social issues, including fair trade, organic integrity, and regenerative agriculture."
-            />
-            </div>
+    <div>
+      <div className="bg-repeat bg-[url('/assets/plant-1.jpg')]">
+        <ContextLayout>
+          <div className="spotlight-page grid grid-cols-3 pt-20">
+            <div className="w-full"></div>
+            <div className="grid grid-rows-1">
+              <div className="pb-5">
+                <div className="bg-slate-100/[.80] mx-auto p-4 rounded-lg text-center">
+                  Coverage of companies and startups finding groundbreaking sustainability solutions
+                </div>
+              </div>
+              <div className="pb-5">
+                <BrandCard
+                  image="assets/brands/dr-bronners.png"
+                  name="Dr. Bronners"
+                  description="Dr. Bronner's is a renowned brand celebrated for its organic and fair-trade personal care products, especially its versatile liquid and bar soaps. Founded in 1948 by Emanuel Bronner, the company has maintained a strong commitment to environmental sustainability and social responsibility. Their products are made with natural, biodegradable ingredients and come in eco-friendly packaging. Dr. Bronner's is also known for its advocacy on various social issues, including fair trade, organic integrity, and regenerative agriculture."
+                />
+              </div>
 
-            <div className="pb-5">
-            <BrandCard
-              image="assets/brands/kosas.png"
-              name="Kosas"
-              description="Kosas is a contemporary beauty brand dedicated to creating clean, skin-loving makeup that enhances natural beauty. Founded by Sheena Yaitanes, Kosas focuses on formulating products with high-quality, non-toxic ingredients that deliver exceptional performance without compromising skin health. Known for its innovative and inclusive approach, the brand offers a range of cosmetics that cater to diverse skin tones and types. Kosas embodies a modern, effortless aesthetic, promoting a beauty philosophy that celebrates individuality and self-expression."
-            />
-            </div>
+              <div className="pb-5">
+                <BrandCard
+                  image="assets/brands/kosas.png"
+                  name="Kosas"
+                  description="Kosas is a contemporary beauty brand dedicated to creating clean, skin-loving makeup that enhances natural beauty. Founded by Sheena Yaitanes, Kosas focuses on formulating products with high-quality, non-toxic ingredients that deliver exceptional performance without compromising skin health. Known for its innovative and inclusive approach, the brand offers a range of cosmetics that cater to diverse skin tones and types. Kosas embodies a modern, effortless aesthetic, promoting a beauty philosophy that celebrates individuality and self-expression."
+                />
+              </div>
 
-            <div className="pb-5">
-              <div className="bg-slate-100/[.80] mx-auto p-4 rounded-lg text-center">
-                More brands to spotlight coming soon!
+              <div className="pb-5">
+                <div className="bg-slate-100/[.80] mx-auto p-4 rounded-lg text-center">
+                  More brands to spotlight coming soon!
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </ContextLayout>
+        </ContextLayout>
+      </div>
     </div>
   );
 };
