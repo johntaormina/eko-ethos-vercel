2be38e4b8e6b54b3d1769aa1644c01f069754db4
johntaormina <jtaor96@gmail.com>
2024-06-25 11:34:21 -0400

    adding

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import { Plant } from "components/main/plant";
 import ContextLayout  from "layouts/ContextLayout";
 
 export default function Home() {
+  const homeText = "Cultivating a Kinder Earth."
   return (
     <ContextLayout>
       <div className="flex flex-col h-screen">
-        <Plant motto="Cultivating a Kinder Earth" />
+      <Plant />
+      <div className="flex items-center justify-center absolute inset-0 mt-12 m-20">
+        <div className="bg-white p-4 bg-opacity-40 rounded-md">
+          <h1 className="text-center text-5xl font-bold opacity-100">{homeText}</h1>
+        </div>
       </div>
+    </div>
     </ContextLayout>
   );
 }
